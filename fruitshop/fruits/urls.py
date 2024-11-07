5ac00806758f7dee230643b2bffefb1c255a9274
Uvesh <uveshpathan665@gmail.com>
2024-11-07 11:23:12 +0530

    uvesh 7/11

--- fruitshop/fruits/urls.py
+++ fruitshop/fruits/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('',views.index,name="index"),
-    path('login',views.cus_login,name="login")
-]
+    path('login',views.cus_login,name="login"),
+    path('logout',views.cus_logout,name="logout"),
+    path('products',views.products,name="products"),
+    path('product_detail/',views.shop_detail,name="product_detail"),
+    path('contact',views.contact,name="contact"),
+    path('cart',views.cart,name="cart"),
+    path('checkout',views.checkout,name="checkout"),
+    path('testimonial',views.testimonial,name="testimonial"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- fruitshop/fruits/views.py
+++ fruitshop/fruits/views.py
@@ -1,8 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate , login , logout
+from .models import *
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def cus_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return redirect('/login')
     return render(request, 'login.html')
+
+
+def cus_logout(request):
+    logout(request)
+    return redirect('/')
+
+def products(request):
+
+    products = Product.objects.all()
+    return render(request, 'shop.html',{'products':products})
+
+def shop_detail(request):
+    return render(request, 'shop-detail.html')
+
+
+def cart(request):
+    return render(request, 'cart.html')
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def testimonial(request):
+    return render(request,'testimonial.html')
+
+def contact(request):
+    return render(request, 'contact.html')
